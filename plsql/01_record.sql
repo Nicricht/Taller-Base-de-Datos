252b3c32980662458024d53eef721d2d7c45f3ca
Nicolás Iván Vega Linero <nic.vegal@duocuc.cl>
2026-09-14 00:51:08 -0300

    refactor(plsql): adaptar RECORD a denominacion carrera

--- plsql/01_record.sql
+++ plsql/01_record.sql
@@ -2,11 +2,12 @@ SET SERVEROUTPUT ON;
 
 DECLARE
     TYPE t_oferta IS RECORD (
-        carrera        CARRERA.nombre%TYPE,
-        institucion    INSTITUCION.nombre%TYPE,
-        comuna         COMUNA.nombre%TYPE,
-        modalidad      MODALIDAD.nombre%TYPE,
-        jornada        JORNADA.nombre%TYPE,
+        carrera         DENOMINACION_CARRERA.nombre%TYPE,
+        nivel_carrera   NIVEL_CARRERA.nombre%TYPE,
+        institucion     INSTITUCION.nombre%TYPE,
+        comuna          COMUNA.nombre%TYPE,
+        modalidad       MODALIDAD.nombre%TYPE,
+        jornada         JORNADA.nombre%TYPE,
         valor_matricula PLAN_OFERTA.valor_matricula%TYPE,
         valor_arancel   PLAN_OFERTA.valor_arancel%TYPE
     );
@@ -15,7 +16,8 @@ DECLARE
     v_id_oferta OFERTA_ACADEMICA.id_oferta%TYPE := 1;
 BEGIN
     SELECT
-        c.nombre,
+        dc.nombre,
+        nc.nombre,
         i.nombre,
         co.nombre,
         m.nombre,
@@ -24,6 +26,7 @@ BEGIN
         po.valor_arancel
     INTO
         v_oferta.carrera,
+        v_oferta.nivel_carrera,
         v_oferta.institucion,
         v_oferta.comuna,
         v_oferta.modalidad,
@@ -32,6 +35,8 @@ BEGIN
         v_oferta.valor_arancel
     FROM OFERTA_ACADEMICA o
     JOIN CARRERA c ON c.id_carrera = o.id_carrera
+    JOIN DENOMINACION_CARRERA dc ON dc.id_denominacion = c.id_denominacion
+    JOIN NIVEL_CARRERA nc ON nc.id_nivel_carrera = c.id_nivel_carrera
     JOIN INSTITUCION i ON i.id_institucion = o.id_institucion
     JOIN COMUNA co ON co.id_comuna = o.id_comuna
     JOIN MODALIDAD m ON m.id_modalidad = o.id_modalidad
@@ -41,6 +46,7 @@ BEGIN
       AND ROWNUM = 1;
 
     DBMS_OUTPUT.PUT_LINE('Carrera: ' || v_oferta.carrera);
+    DBMS_OUTPUT.PUT_LINE('Nivel: ' || v_oferta.nivel_carrera);
     DBMS_OUTPUT.PUT_LINE('Institucion: ' || v_oferta.institucion);
     DBMS_OUTPUT.PUT_LINE('Comuna: ' || v_oferta.comuna);
     DBMS_OUTPUT.PUT_LINE('Modalidad: ' || v_oferta.modalidad);
