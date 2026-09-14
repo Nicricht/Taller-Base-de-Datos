95fa35caa50955c3d1c5607a696c7d760d524c3b
Nicolás Iván Vega Linero <nic.vegal@duocuc.cl>
2026-09-14 00:51:41 -0300

    refactor(plsql): adaptar excepciones al modelo 3FN

--- plsql/04_excepciones.sql
+++ plsql/04_excepciones.sql
@@ -1,15 +1,18 @@
 SET SERVEROUTPUT ON;
 
 DECLARE
-    v_nombre CARRERA.nombre%TYPE;
+    v_nombre DENOMINACION_CARRERA.nombre%TYPE;
+    v_nivel NIVEL_CARRERA.nombre%TYPE;
     v_id_carrera NUMBER := -999;
 BEGIN
-    SELECT nombre
-    INTO v_nombre
-    FROM CARRERA
-    WHERE id_carrera = v_id_carrera;
+    SELECT dc.nombre, nc.nombre
+    INTO v_nombre, v_nivel
+    FROM CARRERA c
+    JOIN DENOMINACION_CARRERA dc ON dc.id_denominacion = c.id_denominacion
+    JOIN NIVEL_CARRERA nc ON nc.id_nivel_carrera = c.id_nivel_carrera
+    WHERE c.id_carrera = v_id_carrera;
 
-    DBMS_OUTPUT.PUT_LINE('Carrera encontrada: ' || v_nombre);
+    DBMS_OUTPUT.PUT_LINE('Carrera encontrada: ' || v_nombre || ' (' || v_nivel || ')');
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
         DBMS_OUTPUT.PUT_LINE(
