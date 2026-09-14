384fd9dd648115a09440d8c7123fcd5ab08d880d
Nicolás Iván Vega Linero <nic.vegal@duocuc.cl>
2026-09-14 00:49:49 -0300

    refactor(db): actualizar indices del modelo 3FN

--- database/03_indexes.sql
+++ database/03_indexes.sql
@@ -1,14 +1,20 @@
+CREATE INDEX ix_prov_region ON PROVINCIA(id_region);
+CREATE INDEX ix_com_prov ON COMUNA(id_provincia);
 CREATE INDEX ix_inst_tipo ON INSTITUCION(id_tipo_institucion);
-CREATE INDEX ix_nc_ne ON NIVEL_CARRERA(id_nivel_estudio);
+CREATE INDEX ix_acred_inst ON ACREDITACION_INSTITUCION(id_institucion);
 
-CREATE INDEX ix_carrera_area ON CARRERA(id_area);
+CREATE INDEX ix_denom_area ON DENOMINACION_CARRERA(id_area);
+CREATE INDEX ix_nc_ne ON NIVEL_CARRERA(id_nivel_estudio);
+CREATE INDEX ix_carrera_denom ON CARRERA(id_denominacion);
 CREATE INDEX ix_carrera_nivel ON CARRERA(id_nivel_carrera);
 
+CREATE INDEX ix_oferta_inst ON OFERTA_ACADEMICA(id_institucion);
 CREATE INDEX ix_oferta_carrera ON OFERTA_ACADEMICA(id_carrera);
 CREATE INDEX ix_oferta_comuna ON OFERTA_ACADEMICA(id_comuna);
 CREATE INDEX ix_oferta_modalidad ON OFERTA_ACADEMICA(id_modalidad);
 CREATE INDEX ix_oferta_jornada ON OFERTA_ACADEMICA(id_jornada);
 
+CREATE INDEX ix_plan_oferta ON PLAN_OFERTA(id_oferta);
 CREATE INDEX ix_plan_tipo ON PLAN_OFERTA(id_tipo_plan);
 CREATE INDEX ix_plan_arancel ON PLAN_OFERTA(valor_arancel);
 
